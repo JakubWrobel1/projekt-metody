a27accdc1fc380cb0810022c140e5912ded407af
JakubWrobel1 <kubawbl@gmail.com>
2023-12-12 23:40:23 +0100

    test

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,29 @@
 "use strict";
-const wartosc = 2;
-const info = "Hello swiat 22";
-console.log(wartosc);
-console.log(info);
+class SecurityZone {
+    constructor() {
+        this.capacity = 2;
+        this.employeeCount = 0;
+        this.Check = () => {
+            console.log(this.employeeCount);
+        };
+        this.Entry = (employee) => {
+            this.employeeCount++;
+        };
+        const capacity = this.capacity;
+        let employeeCount = this.employeeCount;
+    }
+}
+class EmployeeTypeGuard {
+    constructor(employeeName) {
+        this.employeeName = "";
+        this.employeeName = employeeName;
+    }
+}
+class CardTypeGuard {
+}
+let strefa1 = new SecurityZone();
+let pracownik = new EmployeeTypeGuard("TEST");
+let pracownik2 = new EmployeeTypeGuard("TEST2");
+strefa1.Entry(pracownik);
+strefa1.Entry(pracownik2);
+strefa1.Check();
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,41 @@
-const wartosc: number=2;
-const info:string ="Hello swiat 22";
-console.log(wartosc);
-console.log(info)
+
+class SecurityZone{
+     capacity:number = 2;
+     employeeCount = 0;
+
+     constructor(){
+        const capacity:number = this.capacity;
+        let employeeCount:number = this.employeeCount;
+     }
+
+     Check = ()=>{
+        console.log(this.employeeCount)
+     }
+     
+     Entry = (employee:EmployeeTypeGuard) =>{
+     this.employeeCount++
+}
+
+}
+
+
+class EmployeeTypeGuard{
+    employeeName:string ="";
+    
+    constructor(employeeName:string){
+        this.employeeName = employeeName
+    }
+}
+
+class CardTypeGuard{
+
+}
+
+let strefa1 = new SecurityZone()
+let pracownik = new EmployeeTypeGuard("TEST")
+let pracownik2 = new EmployeeTypeGuard("TEST2")
+
+strefa1.Entry(pracownik)
+strefa1.Entry(pracownik2)
+strefa1.Check()
+
